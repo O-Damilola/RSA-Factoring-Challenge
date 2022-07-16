051464d3dd921a2f650fa0e6772f45d532ed2bfd
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-07-16 14:18:50 +0100

    rsa task review

--- rsa.py
+++ rsa.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-import math
+
+import sys
 
 def prime_product(n):
-    numz = math.floor(n/3)
-    n_list = list(range(2, numz+1))
-    primelist = [2,3,5,7]
-    for item in n_list:
-        if (item % 2 != 0 and item % 3 != 0 and item % 5 != 0 and item % 7 != 0):
-            primelist.append(item)
-    print(primelist)        
-    comp = []
-    i = 0
-    while i <= len(primelist) and len(comp) < 2:
-        for item in primelist:
-            if primelist[i] * item == n:
-                comp.append(primelist[i])
-                comp.append(item)
-        i = i+1
-    print("{:d} = {:d}*{:d}".format(n, comp[1], comp[0], end='\n'))
-            
-
+    if n % 2 == 0:
+            print("{}={}*2".format(n, n//2))
+    else:
+        for value in range(3, n, 2):
+            if n % value == 0:
+                quotient = n//value
+                for valz in range(3, quotient, 2):
+                    if quotient % valz == 0 or value % valz == 0:
+                        break
+                print("{}={}*{}".format(n, quotient, value))
+                break
+   
 def main():
-    with open(sys.argv[1]) as f:
-        for num in f:
+    with open(sys.argv[1]) as book:
+        for num in book:
             prime_product(int(num))
 
 if __name__ == "__main__" :
     main()
+
