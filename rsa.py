3a5ce608126869f7353629cf16d33d140867c0e2
odamilolaakinyele <odamilolaakinyele@gmail.com>
2022-07-23 14:39:20 +0100

    rsa updated

--- rsa.py
+++ rsa.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-from sys import argv
 
-with open(argv[1]) as f:
-    for line in f:
-        num = int(line)
-        print("{:d}=".format(num), end="")
-        if num % 2 == 0:
-            print("{}*2".format(num//2))
-            continue
-        for i in range(3, num, 2):
-            if num % i == 0:
-                factor = num//i
-                for j in range(3, factor, 2):
-                    if factor % j == 0 or i % j == 0:
+import sys
+
+def prime_product(n):
+    """ Prints the prime product of a number"""
+    if n % 2 == 0:
+            print("{}={}*2".format(n, n//2))
+    else:
+        for value in range(3, n, 2):
+            if n % value == 0:
+                quotient = n//value
+                for valz in range(3, quotient, 2):
+                    if quotient % valz == 0 or value % valz == 0:
                         break
-                print("{}*{}".format(factor, i))
+                print("{}={}*{}".format(n, quotient, value))
                 break
+   
+def main():
+    """ Carries out the prime product process"""
+    with open(sys.argv[1]) as book:
+        for num in book:
+            prime_product(int(num))
+
+if __name__ == "__main__" :
+    main()
+
